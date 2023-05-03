92cac4ec327c82022f81b79ea6cc86a28e6a736f
SensSoft <by4sens@gmail.com>
2023-05-03 21:39:17 +0200

    some dev comment 2

--- app/src/main/java/pl/lsi/gitcheck/MainActivity.kt
+++ app/src/main/java/pl/lsi/gitcheck/MainActivity.kt
@@ -22,6 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 //dev comment 1
+                //dev comment 2
                 ) {
                     Greeting("Android")
                 }
